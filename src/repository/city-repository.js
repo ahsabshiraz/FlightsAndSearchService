647d716d83ea42e6b1100fe28d25946bdb62a3c7
Shiraz <shirazkk1@gmail.com>
2023-09-30 02:34:11 +0530

    minor fixes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -2,7 +2,7 @@ const { City } = require('../models/index');
 
 class CityRepository {
 
-    async createCity({ name }) { 
+    async createCity({ name }) {
         try {
             const city = await City.create({
                 name
@@ -10,7 +10,7 @@ class CityRepository {
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
-            throw {error};
+            throw { error };
         }
     }
 
@@ -24,21 +24,28 @@ class CityRepository {
             return true;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
-            throw {error};
+            throw { error };
         }
     }
 
     async updateCity(cityId, data) {//{name : "Prayagraj"}
         try {
-            const city = await City.update(data, {
-                where: {
-                    id: cityId
-                }
-            });
+            // The below approach also works but will not return updated object
+            // if we are using Pg then returning: true can be used, else not
+            // const city = await City.update(data, {
+            //     where: {
+            //         id: cityId
+            //     },
+            //      
+            // });
+            // for getting updated data in mysql we use the below approach
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
-            throw {error};
+            throw { error };
         }
     }
 
@@ -48,7 +55,7 @@ class CityRepository {
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
-            throw {error};
+            throw { error };
         }
     }
 
