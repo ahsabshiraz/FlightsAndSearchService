e2053181fbb0bcdd889262a9361a64ed0ae85283
Shiraz <shirazkk1@gmail.com>
2023-10-01 04:11:46 +0530

    Added airport seeders

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
+const db = require('./models/index');
+// const {City, Airport} = require('./models/index');
+
 const setupStartServer = async () => {
   //create the express object
   const app = express();
@@ -14,10 +17,12 @@ const setupStartServer = async () => {
 
   app.use('/api', ApiRoutes);
 
-  
-  app.listen(PORT, async() => {
-    console.log(`Server started at ${PORT}`);
 
+  app.listen(PORT, async () => {
+    console.log(`Server started at ${PORT}`);
+    if (process.env.SYNC_DB) {
+      db.sequelize.sync({ alter: true });
+    }
   });
 }
 
