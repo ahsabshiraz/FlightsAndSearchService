8a81ef1b497ec0d7111689de0484f378c5e9d211
Shiraz <shirazkk1@gmail.com>
2023-10-02 02:31:02 +0530

    Added flights search crud with few filters

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -10,12 +10,13 @@ class FlightService {
 
           async createFlight(data) {
                     try {
+
                               if (!compareTime(data.arrivalTime, data.departureTime)) {
                                         throw { error: 'Arrival time cannot be less than departure time' };
                               }
                               const airplane = await this.airplaneRespository.getAirplane(data.airplaneId);
                               const flight = await this.flightrespository.createFlight({
-                                        ...data, totalSeats: airplane.capacity
+                                        ...data, totalSeats: airplane.capacity// spread operater
                               });
                               return flight;
                     } catch (error) {
@@ -25,7 +26,13 @@ class FlightService {
           }
 
           async getFlightData() {
-                    // todo
+                    try {
+                              const flights = await this.flightrespository.getAllFlights(data);
+                              return flights;
+                    } catch (error) {
+                              console.log("Something went wrong at service layer");
+                              throw { error };
+                    }
           }
 }
 
